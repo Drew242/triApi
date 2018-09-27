2fa90410aa5a8fd77f3b2de96fb2c0dbfa0e74d2
drew.conly <drew.conly@cherwell.com>
2018-09-27 14:37:58 -0600

    init repo; endpoint firing

---
+++ Controllers/TrianglesController.cs
@@ -0,0 +1,35 @@
+﻿using triApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace TrianglesApp.Controllers
+{
+	public class TrianglesController : ApiController
+	{
+		Triangle[] triangles = new Triangle[]
+		{
+			new Triangle { Id = 1, Name = "A1", Coordinates = new Coordinates { V1 = new Point(0, 10), V2 = new Point(0, 0), V3 = new Point(10, 10) } },
+			new Triangle { Id = 2, Name = "A2", Coordinates = new Coordinates { V1 = new Point(0, 10), V2 = new Point(0, 0), V3 = new Point(10, 10) } },
+			new Triangle { Id = 3, Name = "A3", Coordinates = new Coordinates { V1 = new Point(0, 10), V2 = new Point(0, 0), V3 = new Point(10, 10) } },
+		};
+
+		public IEnumerable<Triangle> GetAllTriangles()
+		{
+			return triangles;
+		}
+
+		public IHttpActionResult GetTriangle(int id)
+		{
+			var triangle = triangles.FirstOrDefault((p) => p.Id == id);
+			if (triangle == null)
+			{
+				return NotFound();
+			}
+			return Ok(triangle);
+		}
+	}
+}
---
+++ Models/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace triApi.Models
+{
+	public class Triangle
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Coordinates Coordinates { get; set; }
+	}
+
+	public class Coordinates
+	{
+		public Point V1 { get; set; }
+		public Point V2 { get; set; }
+		public Point V3 { get; set; }
+	}
+}
